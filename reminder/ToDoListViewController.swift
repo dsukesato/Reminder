36bec28691a285e91fdd546a620aeb345be6d738
daisuke sato <satodsuke@gmail.com>
2018-12-24 15:00:29 +0900

    first commit

---
+++ reminder/ToDoListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ToDoListViewController.swift
+//  reminder
+//
+//  Created by 佐藤大介 on 2018/12/20.
+//  Copyright © 2018 佐藤大介. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ reminder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  reminder
+//
+//  Created by 佐藤大介 on 2018/12/19.
+//  Copyright © 2018 佐藤大介. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
